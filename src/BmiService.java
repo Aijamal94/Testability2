daefa26152a474bd57606445df15a3ff76d7a399
Aizhamal <aijashka79@gmail.com>
2024-05-08 16:34:04 +0500

    Initial commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class BmiService {
+public class BmiService {
+    public int calculate(double weight, double height) {
+        double bmiIndex = weight / (height * height); // Расчет BMI индекса
+        int bmiIndexInt = (int) bmiIndex; // Преобразование в целое число
+
+        return bmiIndexInt;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double weight = 98; // Вес в кг
+        double height = 1.87; // Рост в метрах
+
+        BmiService bmiService = new BmiService();
+        int bmiIndex = bmiService.calculate(weight, height);
+
+        System.out.println("BMI индекс: " + bmiIndex);
+
     }
 }
